a135d3713761cca95d4c71cb1836e05d3b7d6990
Corkerro <74060229+Corkerro@users.noreply.github.com>
2023-07-07 20:41:50 +0300

    add

--- js/app.js
+++ js/app.js
@@ -43,6 +43,104 @@
         }));
         isTouch = true;
     }
+    const coursesCard = document.querySelectorAll(".courses__card");
+    coursesCard.forEach((course => {
+        let timeoutId, timeoutId2;
+        const text = course.querySelector(".courses__card_text");
+        const span = text.querySelector("span");
+        course.addEventListener("mouseover", (() => {
+            text.style.height = "100%";
+            text.style.left = "12%";
+            timeoutId = setTimeout((() => {
+                text.style.width = "140%";
+                text.style.left = "-20%";
+                span.style.scale = "1.6";
+                course.classList.add("hover");
+            }), 300);
+            timeoutId2 = setTimeout((() => {
+                course.classList.add("hover");
+            }), 300);
+        }));
+        course.addEventListener("mouseout", (() => {
+            clearTimeout(timeoutId);
+            clearTimeout(timeoutId2);
+            text.style.height = "74px";
+            text.style.width = "calc(100% - 120px)";
+            text.style.left = "-15px";
+            span.style.scale = "1";
+            course.classList.remove("hover");
+        }));
+        course.addEventListener("click", (event => {
+            event.preventDefault();
+            const changeLocation = element => {
+                element.classList.add("click");
+                setTimeout((() => {
+                    window.location.href = element.getAttribute("href");
+                    setTimeout((() => {
+                        element.classList.remove("click");
+                        clearTimeout(timeoutId);
+                        clearTimeout(timeoutId2);
+                        text.style.height = "74px";
+                        text.style.width = "calc(100% - 120px)";
+                        text.style.left = "-15px";
+                        span.style.scale = "1";
+                        course.classList.remove("hover");
+                    }), 200);
+                }), 500);
+            };
+            if (course.classList.contains("hover") && !isTouch) changeLocation(course); else {
+                const interval = setInterval((function() {
+                    if (course.classList.contains("hover")) {
+                        clearInterval(interval);
+                        setTimeout((() => {
+                            changeLocation(course);
+                        }), 100);
+                    }
+                }), 200);
+            }
+        }));
+        course.addEventListener("auxclick", (event => {
+            event.preventDefault();
+            const changeLocation = element => {
+                element.classList.add("click");
+                setTimeout((() => {
+                    window.open(element.getAttribute("href"), "_blank");
+                    setTimeout((() => {
+                        element.classList.remove("click");
+                        clearTimeout(timeoutId);
+                        clearTimeout(timeoutId2);
+                        text.style.height = "74px";
+                        text.style.width = "calc(100% - 120px)";
+                        text.style.left = "-15px";
+                        span.style.scale = "1";
+                        course.classList.remove("hover");
+                    }), 100);
+                }), 500);
+            };
+            if (course.classList.contains("hover") && !isTouch) changeLocation(course); else {
+                const interval = setInterval((function() {
+                    if (course.classList.contains("hover")) {
+                        clearInterval(interval);
+                        setTimeout((() => {
+                            changeLocation(course);
+                        }), 100);
+                    }
+                }), 200);
+            }
+        }));
+    }));
+    const partners = document.querySelectorAll(".partners__href");
+    partners.forEach((partner => {
+        console.log(partner);
+        const img = partner.querySelector("img");
+        if (img.naturalHeight / img.naturalWidth >= .38) {
+            img.style.height = "100%";
+            img.style.width = "auto";
+        } else {
+            img.style.height = "auto";
+            img.style.width = "100%";
+        }
+    }));
     gsap.to(".main__perehod_img-1", {
         scrollTrigger: {
             trigger: ".main",
@@ -61,42 +159,6 @@
         y: 50,
         opacity: .9
     });
-    const coursesCard = document.querySelectorAll(".courses__card");
-    coursesCard.forEach((course => {
-        let timeoutId;
-        const text = course.querySelector(".courses__card_text");
-        const span = text.querySelector("span");
-        course.addEventListener("mouseover", (() => {
-            text.style.height = "100%";
-            text.style.left = "17%";
-            timeoutId = setTimeout((() => {
-                text.style.width = "140%";
-                text.style.left = "-20%";
-                span.style.scale = "1.6";
-            }), 300);
-        }));
-        course.addEventListener("mouseout", (() => {
-            clearTimeout(timeoutId);
-            text.style.height = "74px";
-            text.style.width = "calc(100% - 120px)";
-            text.style.left = "-15px";
-            span.style.scale = "1";
-        }));
-    }));
-    const delayedLinks = document.querySelectorAll("[data-delayed]");
-    delayedLinks.forEach((link => {
-        link.addEventListener("click", (event => {
-            event.preventDefault();
-            const delayTime = isTouch ? 300 : 900;
-            link.classList.add("click");
-            setTimeout((() => {
-                window.location.href = link.getAttribute("href");
-                setTimeout((() => {
-                    link.classList.remove("click");
-                }), 200);
-            }), delayTime);
-        }));
-    }));
     window["FLS"] = true;
     isWebp();
 })();
